598ce108c042b657694d794d093efd1cdc5f12f4
Marvin Zhang <ztbsuper@gmail.com>
2015-02-27 16:46:34 +0800

    add daemon monitor and auto-restart in dev task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,25 @@
 var gulp = require('gulp');
 var install = require("gulp-install");
+var nodemon = require("gulp-nodemon");
+var util = require("gulp-util");
 
 gulp.task('install', function () {
   return gulp.src(__dirname + '/src/package.json')
     .pipe(install());
+});
+
+gulp.task('dev', ['install'] ,function () {
+  var config = {
+    script: __dirname + "/src/server.js",
+    ext: 'js json',
+    env: {'NODE_ENV': 'local'}
+  };
+
+  return nodemon(config)
+    .on('change', ['install'], function () {
+      util.log(util.colors.blue("detect js file changed, restart"))
+    })
+    .on('restart', function () {
+      util.log(util.colors.magenta('Restart Service'));
+    });
 });
