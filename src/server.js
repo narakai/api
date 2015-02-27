f4b481fb9eb222d7bb70e4b771bb141206e729c3
Marvin Zhang <ztbsuper@gmail.com>
2015-02-27 16:53:46 +0800

    add winston to log

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("Nuclear launcher detected");
+// initial service
+var Logger = require("winston");
+Logger.info("Nuclear launcher detected");
+var app = require("express")();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+var validator = require("express-validator");
+app.use(validator());
+
+var http = require("http");
+var server = http.createServer(app);
+
+server.listen(9527, function () {
+  Logger.info("boom on " + 9527);
+});
