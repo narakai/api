fecd1d8c76b325ad9141a54c04c93857b61c7afa
Marvin Zhang <ztbsuper@gmail.com>
2015-02-28 11:24:45 +0800

    add migration and create db script

---
+++ src/migrations/20150228105738-book.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  up: function (migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+    migration.createTable('book', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING(256),
+        allowNull: false
+      },
+      sn: {
+        type: DataTypes.STRING(256)
+      },
+      summary:{
+        type: DataTypes.TEXT
+      },
+      userId:{
+        type: DataTypes.INTEGER
+      }
+    });
+    done();
+  },
+
+  down: function (migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    done();
+  }
+};
