94ecbec09a085280c22e5aaf42c27fad17b26722
Marvin Zhang <ztbsuper@gmail.com>
2015-03-02 00:09:44 +0800

    fix ci

--- test/specs/books.spec.js
+++ test/specs/books.spec.js
@@ -9,7 +9,6 @@ describe('[Books API] ', function () {
         sn: "ISBN-321312-3213-123",
         summary: ""
       };
-      console.log("request" + config.api + "/books");
       request.post(config.api + "/books", {json: postData}, function (error, response, body) {
         expect(response.statusCode).toBe(201);
         expect(body.id).toBeDefined();
